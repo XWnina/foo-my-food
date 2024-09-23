51eefaf0f9ed67ccd6ad709733ec2bfac428999d
Charlie Fang <charlie.fang0130@gmail.com>
2024-09-23 15:09:19 -0400

    initialization login, create account page

--- frontend/lib/login_page.dart
+++ frontend/lib/login_page.dart
@@ -1,75 +1,146 @@
 import 'package:flutter/material.dart';
-import 'main.dart'; // 导入 MyHomePage
+import 'main.dart';
+import 'create_account_page.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
-
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  final TextEditingController _usernameController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-
-  void _login() {
-    // 获取输入的用户名和密码
-    String username = _usernameController.text;
-    String password = _passwordController.text;
-
-    if (username.isNotEmpty && password.isNotEmpty) {
-      // 用户名和密码有效时，跳转到 MyHomePage
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const MyHomePage(title: 'Foo my food Demo')),
-      );
-    } else {
-      // 提示用户输入用户名和密码
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Please enter both username and password')),
-      );
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // 用户名输入框
-            TextField(
-              controller: _usernameController,
-              decoration: const InputDecoration(
-                labelText: 'Username',
-                border: OutlineInputBorder(),
+      backgroundColor: const Color.fromARGB(255, 18, 32, 47),
+      body: Center(
+        child: Container(
+          width: 360,
+          height: 640,
+          decoration: BoxDecoration(
+            color: const Color(0xFFD1E7FE),
+            border: Border.all(width: 1),
+          ),
+          child: Stack(
+            children: [
+              // Logo Image
+              Positioned(
+                left: 81,
+                top: 77,
+                child: Container(
+                  width: 199,
+                  height: 199,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    image: DecorationImage(
+                      image: AssetImage("image/logo3.png"),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
               ),
-            ),
-            const SizedBox(height: 16.0),
-
-            // 密码输入框
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                border: OutlineInputBorder(),
+              // Username Field
+              Positioned(
+                left: 82,
+                top: 324,
+                child: Container(
+                  width: 196,
+                  height: 37,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFFEFFFF),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Username',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
               ),
-              obscureText: true, // 隐藏输入的密码
-            ),
-            const SizedBox(height: 16.0),
-
-            // 登录按钮
-            ElevatedButton(
-              onPressed: _login,
-              child: const Text('Login'),
-            ),
-          ],
+              // Password Field
+              Positioned(
+                left: 82,
+                top: 375,
+                child: Container(
+                  width: 196,
+                  height: 37,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFFFEFFFF),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: 'Password',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              // LOG IN Button
+              Positioned(
+                left: 136,
+                top: 424,
+                child: SizedBox(
+                  width: 89,
+                  height: 29,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF47709B),
+                    ),
+                    onPressed: () {
+                      // 在点击按钮时导航到 MyHomePage
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              const MyHomePage(title: 'Home Page'),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'LOG IN',
+                      style: TextStyle(
+                        color: Color(0xFFE5E5E5),
+                        fontSize: 12,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              // Create Account Button
+              Positioned(
+                left: 116,
+                top: 535,
+                child: SizedBox(
+                  width: 129,
+                  height: 29,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF47709B),
+                    ),
+                    onPressed: () {
+                      // Handle create account action here
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const CreateAccount(),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      'Create Account',
+                      style: TextStyle(
+                        color: Color(0xFFE5E5E5),
+                        fontSize: 13,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
