b06f04a4a8b14d341366d7ac4a034bd7dda6f2a7
Charlie Fang <charlie.fang0130@gmail.com>
2024-09-23 23:27:09 -0400

    Initialize springboot

---
+++ backend/src/main/java/com/foomyfood/foomyfood/FoomyfoodApplication.java
@@ -0,0 +1,13 @@
+package com.foomyfood.foomyfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoomyfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoomyfoodApplication.class, args);
+	}
+
+}
