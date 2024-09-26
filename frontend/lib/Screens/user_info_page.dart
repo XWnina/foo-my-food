7b43934a2dff3491ad97618f64b1f914bafdff34
Charlie Fang <charlie.fang0130@gmail.com>
2024-09-25 22:51:35 -0400

    File structure changes

---
+++ frontend/lib/Screens/user_info_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'set_password_info_varification.dart';
+class UserInfoPage extends StatelessWidget {
+  const UserInfoPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User Information'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'User Name: Charlie Fang',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Email: charlie.fang@example.com',
+              style: TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
