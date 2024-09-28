085da863ff39b67ee2198671996f0cf3edc6d829
zzlbzdml <sheay@connect.ust.hk>
2024-09-27 23:17:06 -0400

    add basic home page
    add basic home page

---
+++ frontend/lib/models/ingredient.dart
@@ -0,0 +1,17 @@
+class Ingredient {
+  final String name;
+  final String category;
+  final String storageMethod;
+  final int quantity;
+  final DateTime expirationDate;
+  final FileImage;
+
+  Ingredient({
+    required this.name,
+    required this.category,
+    required this.storageMethod,
+    required this.quantity,
+    required this.expirationDate,
+    this.image,
+  });
+}
