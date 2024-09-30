572e7c4fdb9ef959bdf342a488ba766ebad09691
Nina Wu <wu1765@purdue.edu>
2024-09-29 22:15:15 -0400

    vegetable class connection and  initialization function realized

--- backend/src/main/java/com/foomyfood/foomyfood/database/repository/VegetableRepository.java
+++ backend/src/main/java/com/foomyfood/foomyfood/database/repository/VegetableRepository.java
@@ -1,11 +1,12 @@
 package com.foomyfood.foomyfood.database.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;  
+import org.springframework.stereotype.Repository;  // 引入User实体类
 
 import com.foomyfood.foomyfood.database.Vegetable;
 
 @Repository
 public interface VegetableRepository extends JpaRepository<Vegetable, Long> {
+    
 }
 
