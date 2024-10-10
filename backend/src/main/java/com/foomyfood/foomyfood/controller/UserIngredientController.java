efe7027cc0abfbd98a051bc61c76c18ba4e6e6b7
zzlbzdml <sheay@connect.ust.hk>
2024-10-10 11:42:39 -0400

    123

--- backend/src/main/java/com/foomyfood/foomyfood/controller/UserIngredientController.java
+++ backend/src/main/java/com/foomyfood/foomyfood/controller/UserIngredientController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/user-ingredients")
+@RequestMapping("/api/user_ingredients")
 public class UserIngredientController {
 
     @Autowired
