9c21ff072531ca0392b4eaac89ce123df8851167
Nina Wu <wu1765@purdue.edu>
2024-10-11 14:44:59 -0400

    tester and user ingredient service updates

--- backend/src/main/java/com/foomyfood/foomyfood/database/db_service/UserIngredientService.java
+++ backend/src/main/java/com/foomyfood/foomyfood/database/db_service/UserIngredientService.java
@@ -1,12 +1,15 @@
 package com.foomyfood.foomyfood.database.db_service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.foomyfood.foomyfood.database.Ingredient;
 import com.foomyfood.foomyfood.database.UserIngredient;
+import com.foomyfood.foomyfood.database.db_repository.IngredientRepository;
 import com.foomyfood.foomyfood.database.db_repository.UserIngredientRepository;
 
 @Service
@@ -14,6 +17,9 @@ public class UserIngredientService {
 
     @Autowired
     private UserIngredientRepository userIngredientRepository;
+
+    @Autowired
+    private IngredientRepository ingredientRepository;
 
     // Get all user ingredients by userId
     public List<UserIngredient> getAllUserIngredients(Long userId) {
@@ -51,5 +57,39 @@ public class UserIngredientService {
         } else {
             throw new RuntimeException("UserIngredient not found for userId " + userId + " and ingredientId " + ingredientId);
         }
+    }
+
+    public List<List<Object>> getUserIngredientsDetails(Long userId) {
+        // Step 1: Get all ingredientIds for the given userId
+        List<UserIngredient> ingredientIds = userIngredientRepository.findAllByUserId(userId);
+
+        // Step 2: Retrieve the details of the ingredients by ingredientIds from ingredients table
+        List<Ingredient> ingredients = ingredientRepository.findAll();
+
+        // Step 3: Convert the ingredients into a list of lists
+        List<List<Object>> ingredientDetailsList = new ArrayList<>();
+
+        for (Ingredient ingredient : ingredients) {
+            // Add ingredient properties to inner list
+            List<Object> ingredientDetails = new ArrayList<>();
+            ingredientDetails.add(ingredient.getIngredientId());
+            ingredientDetails.add(ingredient.getName());
+            ingredientDetails.add(ingredient.getStorageMethod());
+            ingredientDetails.add(ingredient.getBaseQuantity());
+            ingredientDetails.add(ingredient.getCalories());
+            ingredientDetails.add(ingredient.getCarbohydrates());
+            ingredientDetails.add(ingredient.getCategory());
+            ingredientDetails.add(ingredient.getCreatedBy());
+            ingredientDetails.add(ingredient.getExpirationDate());
+            ingredientDetails.add(ingredient.getFat());
+            ingredientDetails.add(ingredient.getFiber());
+            ingredientDetails.add(ingredient.getImageURL());
+            ingredientDetails.add(ingredient.getIsUserCreated());
+
+            // Add the inner list to the outer list
+            ingredientDetailsList.add(ingredientDetails);
+        }
+
+        return ingredientDetailsList;  // List of list with ingredient details
     }
 }
