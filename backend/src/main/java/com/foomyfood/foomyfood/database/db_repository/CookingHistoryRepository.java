d20f144872aab97f14b2605048a974abe0af2513
Nina Wu <wu1765@purdue.edu>
2024-11-10 17:43:43 -0500

    fixing the relations between recipe table and cooking history, function of delete recipe works

--- backend/src/main/java/com/foomyfood/foomyfood/database/db_repository/CookingHistoryRepository.java
+++ backend/src/main/java/com/foomyfood/foomyfood/database/db_repository/CookingHistoryRepository.java
@@ -4,14 +4,25 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.foomyfood.foomyfood.database.CookingHistory;
 
+import jakarta.transaction.Transactional;
+
 @Repository
 public interface CookingHistoryRepository extends JpaRepository<CookingHistory, Long> {
-    List<CookingHistory> findByUserId(Long userId);
-    List<CookingHistory> findByRecipeId(Long recipeId);
-    long countByRecipeIdAndCookingDateBetween(Long recipeId, LocalDate startDate, LocalDate endDate);
-}
 
+    List<CookingHistory> findByUserId(Long userId);
+
+    List<CookingHistory> findByRecipeId(Long recipeId);
+
+    long countByRecipeIdAndCookingDateBetween(Long recipeId, LocalDate startDate, LocalDate endDate);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM CookingHistory ch WHERE ch.recipe.id = :recipeId")
+    void deleteByRecipeId(Long recipeId);
+}
