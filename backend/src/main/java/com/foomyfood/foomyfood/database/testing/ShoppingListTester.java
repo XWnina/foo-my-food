7539ff5a4419d9a0f93a74cf6db296b8cc439047
Nina Wu <wu1765@purdue.edu>
2024-11-19 19:37:59 -0500

    recipe download as picture done

--- backend/src/main/java/com/foomyfood/foomyfood/database/testing/ShoppingListTester.java
+++ backend/src/main/java/com/foomyfood/foomyfood/database/testing/ShoppingListTester.java
@@ -5,12 +5,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
 
 import com.foomyfood.foomyfood.database.ShoppingList;
 import com.foomyfood.foomyfood.database.db_service.ShoppingListService;
 
-@Component
+// @Component
 public class ShoppingListTester implements CommandLineRunner {
 
     @Autowired
